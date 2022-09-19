fde78db04faebf6a755ae18e1a2e8902ca1c358b
zaidkamil <xaidmetamorphos@gmail.com>
2022-09-19 11:59:28 +0530

    menubar and links

--- app.py
+++ app.py
@@ -6,11 +6,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-#froute
+# froute
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+
 if __name__ == '__main__':
-  app.run(host='127.0.0.1', port=8000, debug=True)
- 
+    app.run(host='127.0.0.1', port=8000, debug=True)
