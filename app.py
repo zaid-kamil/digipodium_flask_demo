021ff7248027d5762d1c7bc9bf3bb3cfd35f6c46
zaidkamil <xaidmetamorphos@gmail.com>
2022-09-23 11:50:54 +0530

    register

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, request, redirect, flash, session
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(64), nullable=False)
+    created_on = db.Column(db.DateTime, default=datetime.now)
+
+    def __str__(self):
+        return f'{self.username}({self.id})'
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/app.sqlite'
+    app.config['SQLALCHEMY_ECHO'] = True
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.secret_key = 'supersecretkeythatnooneknows'
+    db.init_app(app)
+    return app
+
+app = create_app()
 
 @app.route('/')
 def index():
@@ -11,9 +34,33 @@ def index():
 def login():
     return render_template('login.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def register():
-    return render_template('register.html')
+    errors = []
+    if request.method == 'POST': # if form was submitted
+        username = request.form.get('username')
+        email = request.form.get('email')
+        pwd = request.form.get('password')
+        cpwd = request.form.get('confirmpass')
+        print(username, email, pwd, cpwd)
+        if username and email and pwd and cpwd:
+            if len(username)<2:
+                errors.append("Username is too small")
+            if len(email) < 11 or '@' not in email:
+                errors.append("Email is invalid")
+            if len(pwd) < 6:
+                errors.append("Password should be 6 or more chars")
+            if pwd != cpwd:
+                errors.append("passwords do not match")
+            if len(errors) == 0:
+                user = User(username=username, email=email, password=pwd)
+                db.session.add(user)
+                db.session.commit()
+                flash('user account created','success')
+                return redirect('/login')
+        else:
+            errors.append('Fill all the fields')
+    return render_template('register.html', error_list=errors)
 
 
 if __name__ == '__main__':
